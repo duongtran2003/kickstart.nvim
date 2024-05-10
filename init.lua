dbafc0ea36689b0fe536e564a2c2d5c5ac549f14
duongtran2003 <dt005250@gmail.com>
2024-05-10 22:24:50 +0700

    added barbar

--- init.lua
+++ init.lua
@@ -547,6 +547,8 @@ require('lazy').setup({
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
+        --python server
+        pyright = {},
         -- But for many setups, the LSP (`tsserver`) will work just fine
         tsserver = {},
         --tailwindcss
